6259e9383f9a57c6afd4437183a6a791896da21b
ddunn40495 <ddunn40495@gmail.com>
2020-11-01 02:19:30 -0500

    Deploy

--- server.js
+++ server.js
@@ -14,19 +14,8 @@ require("dotenv").config();
 //      Cors
 // =======================================
 const cors = require("cors");
-const whitelist = ["http://localhost:3000", "https://homeroomclass.net"];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
 
-app.use(cors(corsOptions));
-
+app.use(cors());
 // =======================================
 //      CONTROLLERS
 // =======================================
