5aad0b8ee5ef34ad7c95296793a970b4345f169d
ddunn40495 <ddunn40495@gmail.com>
2021-01-06 11:52:33 -0500

    database update

---
+++ models/data/json.js
@@ -0,0 +1,7 @@
+const object = {
+  teacher_first_name: "big",
+  teacher_last_name: "boss",
+  teacher_user_name: "bboss",
+  teacher_password: "password",
+  teacher_email: "bboss@nomail.com",
+};
